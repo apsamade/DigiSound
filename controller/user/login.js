3fbcd412c7c59f9f39bd473a9d47e56adb396a92
apsamade <bouderga.abdessamade@gmail.com>
2023-11-21 21:56:05 +0100

    maj

--- controller/user/login.js
+++ controller/user/login.js
@@ -6,10 +6,15 @@ exports.getLogin = (req, res, next)=>{
 exports.postLogin = async (req, res, next)=>{
     const {pseudo, mdp} = req.body
     const user = await User.findOne({pseudo: pseudo, mdp: mdp})
-    if(user){
-        req.session.user = user;
-        res.redirect('/')
-    }else{
-        res.render('login', {err: 'Erreur lors de la tentaive de connexion !'})
+    try {
+        if(user){
+            req.session.user = user;
+            res.redirect('/')
+        }else{
+            res.render('login', {err: 'Erreur lors de la tentaive de connexion !'})
+        }        
+    } catch (error) {
+        console.log(error)
     }
+
 }
