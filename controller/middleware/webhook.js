07a68afe61e406d02c432c292f36dc3d0dfe8f3e
apsamade <bouderga.abdessamade@gmail.com>
2023-11-21 22:20:39 +0100

    maj

--- controller/middleware/webhook.js
+++ controller/middleware/webhook.js
@@ -7,21 +7,13 @@ const User = require('../../models/user')
 
 
 exports.handleWebhook = async (req, res, next) => {
-    const user = req.session.user;
-    console.log(req.session)
     const sig = req.headers['stripe-signature'];
     
-    console.log(user)
-    // const panier = await Panier.findOne({userId: user._id, payer: false})
     let event;
 
     try {
         event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-    
-        // Ajoutez un log ici pour afficher l'utilisateur après la construction de l'événement
-        console.log('User (after event construction):', req.session.user);
     } catch (err) {
-        console.error('Erreur lors de la construction de l\'événement:', err);
         res.status(400).send(`Webhook Error: ${err.message}`);
         return;
     }
@@ -31,10 +23,6 @@ exports.handleWebhook = async (req, res, next) => {
         case 'payment_intent.succeeded':
             const paymentIntentSucceeded = event.data.object;
             console.log('paiement effectuer avec succès', paymentIntentSucceeded.status)
-            // if(user && panier){
-            //     panier.payer = true;
-            // }
-            // await panier.save()
             break;
         // ... handle other event types
         default:
