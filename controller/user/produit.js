18a2a7d0e2fe004e9b0a8a5e4f8b31b9c01dd74e
apsamade <bouderga.abdessamade@gmail.com>
2023-11-21 21:04:20 +0100

    maj

--- controller/user/produit.js
+++ controller/user/produit.js
@@ -16,6 +16,7 @@ exports.getProduit =  async (req, res, next)=>{
 }
 exports.postProduit = async (req, res, next)=>{
     const productId = req.params.id
+    const user = req.session.user
     const produit = await Product.findById(productId)
     try {
         const panierExisting = await Panier.findOne({userId: user._id})
