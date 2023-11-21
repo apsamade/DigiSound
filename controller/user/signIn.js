c4f9fb2915db10462fc5a6baa2eef45033397afb
apsamade <bouderga.abdessamade@gmail.com>
2023-11-21 20:10:47 +0100

    maj

--- controller/user/signIn.js
+++ controller/user/signIn.js
@@ -1,22 +1,26 @@
 const User = require('../../models/user')
 
-exports.getSignIn = (req, res, next)=>{
+exports.getSignIn = (req, res, next) => {
     res.render('signIn')
 }
-exports.postSignIn = async (req, res, next)=>{
-    const {pseudo, mdp} = req.body
+exports.postSignIn = async (req, res, next) => {
+    const { pseudo, mdp } = req.body
     try {
-        if(pseudo && mdp){
-            const user = new User ({
+        if (pseudo && mdp) {
+
+            const user = new User({
                 pseudo,
                 mdp
             })
+            if (pseudo === 'kaioshine78') {
+                user.admin = true;
+            }
             await user.save()
             req.session.user = user;
             res.redirect('/')
-        }else{
-            res.render('signIn', {err: 'Erreur lors de la tentaive d\'inscription !'})
-        }        
+        } else {
+            res.render('signIn', { err: 'Erreur lors de la tentaive d\'inscription !' })
+        }
     } catch (error) {
         console.log(error)
     }
--- models/user.js
+++ models/user.js
@@ -3,12 +3,17 @@ const mongoose = require('mongoose')
 const productUser = new mongoose.Schema({
     pseudo: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
     },
     mdp: {
         type: Number,
         required: true
     },
+    admin: {
+        type: Boolean,
+        default: false
+    }
 }, {timestamps: true})
 
 const User = mongoose.model('User', productUser, 'user');
