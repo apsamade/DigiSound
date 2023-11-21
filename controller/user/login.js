4ef646184c6a3227dda9a0882d7aea95b6e22ded
apsamade <bouderga.abdessamade@gmail.com>
2023-11-21 20:06:57 +0100

    maj

--- controller/user/login.js
+++ controller/user/login.js
@@ -1,13 +1,12 @@
+const User = require('../../models/user')
+
 exports.getLogin = (req, res, next)=>{
     res.render('login')
 }
-exports.postLogin = (req, res, next)=>{
+exports.postLogin = async (req, res, next)=>{
     const {pseudo, mdp} = req.body
-    if(pseudo === 'kaioshine78' && mdp === 'samade782'){
-        const user = {
-            pseudo,
-            mdp
-        }
+    const user = await User.findOne({pseudo: pseudo, mdp: mdp})
+    if(user){
         req.session.user = user;
         res.redirect('/')
     }else{
