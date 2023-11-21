f808cbc6754c4633469b9f81fed849f7866cd808
apsamade <bouderga.abdessamade@gmail.com>
2023-11-21 20:04:54 +0100

    maj

--- controller/user/signIn.js
+++ controller/user/signIn.js
@@ -1,17 +1,24 @@
+const User = require('../../models/user')
+
 exports.getSignIn = (req, res, next)=>{
     res.render('signIn')
 }
 exports.postSignIn = async (req, res, next)=>{
     const {pseudo, mdp} = req.body
-    if(pseudo && mdp){
-        const user = {
-            pseudo,
-            mdp
-        }
-        await user.save()
-        req.session.user = user;
-        res.redirect('/')
-    }else{
-        res.render('signIn', {err: 'Erreur lors de la tentaive d\'inscription !'})
+    try {
+        if(pseudo && mdp){
+            const user = new User ({
+                pseudo,
+                mdp
+            })
+            await user.save()
+            req.session.user = user;
+            res.redirect('/')
+        }else{
+            res.render('signIn', {err: 'Erreur lors de la tentaive d\'inscription !'})
+        }        
+    } catch (error) {
+        console.log(error)
     }
+
 }
