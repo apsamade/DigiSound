2e08ddc42cdd6c95a22ff9ec3b9a84d82fc70306
apsamade <bouderga.abdessamade@gmail.com>
2023-11-21 17:39:27 +0100

    maj

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const webhookController = require('../controller/middleware/webhook');
 
-router.post('/webhook', webhookController.handleWebhook);
+router.post('/webhook', express.raw({ type: 'application/json' }), webhookController.handleWebhook);
 
 module.exports = router;
