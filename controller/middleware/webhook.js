b6e605473269f43a6b621fcd0ee47947b43d7252
apsamade <bouderga.abdessamade@gmail.com>
2023-11-22 20:53:35 +0100

    maj

--- controller/middleware/webhook.js
+++ controller/middleware/webhook.js
@@ -24,7 +24,7 @@ exports.handleWebhook = async (req, res, next) => {
             const panier = await Panier.findOne({payementIntentId: paymentIntentSucceeded.id})
             panier.payer = true;
             await panier.save()
-            res.redirect('/')
+            res.redirect(`/panier/${panier._id}/confirmation-du-payement`)
             break;
         // ... handle other event types
         default:
