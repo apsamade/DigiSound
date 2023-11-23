0cf0900eef92755893515ae6b327e6df372e8926
apsamade <bouderga.abdessamade@gmail.com>
2023-11-23 21:13:41 +0100

    maj

--- controller/user/confirmPayement.js
+++ controller/user/confirmPayement.js
@@ -1,14 +1,16 @@
 const Panier = require('../../models/panier')
-
+const stripe = require("stripe")(process.env.SECRET_KEY_STRIPE);
 
 exports.getConfirmPayement =  async (req, res, next)=>{
     const user = req.session.user
     const panierId = req.params.id
+    const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+    console.log(session)
     const panier = await Panier.findById(panierId)
 
     try {
         if(panier){
-            console.log(req.query.session_id)
+            if('session payé !', session.status)
             res.render('confirmPayement', {user, panier})
         }else{
             res.render('confirmPayement', {user, panier, err: 'panier non payer une erreur est survenue !'})
