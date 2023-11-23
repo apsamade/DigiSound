dda2df9e9225018e975c01c60f48cdb8ad44cd23
apsamade <bouderga.abdessamade@gmail.com>
2023-11-23 18:09:47 +0100

    maj

--- controller/middleware/webhook.js
+++ controller/middleware/webhook.js
@@ -23,7 +23,7 @@ exports.handleWebhook = async (req, res, next) => {
     }
 
     // Handle the checkout.session.completed event
-    if (event.type === 'checkout.session.completed') {
+    if (event.type === 'checkout.session.async_payment_succeeded') {
         // Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
         const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
             event.data.object.id,
